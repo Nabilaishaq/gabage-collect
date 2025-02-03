cfb2193ef9645b2ffd6d416d76842653e3283b97
Nabilaishaq <nblayshishaq@gmail.com>
2025-02-03 09:54:38 +0100

    MANIPULATION

--- Strings/Manipulation/Program.cs
+++ Strings/Manipulation/Program.cs
@@ -1,37 +1,54 @@
-﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
-// Example file for manipulating string content
+﻿using System;
 
-string str1 = "The quick brown fox jumps over the lazy dog.";
-string str2 = "This is a string";
-string str3 = "THIS is a STRING";
-string[] strs = {"one", "two", "three", "four"};
+class Program {
+    static void Main() {
+        string str1 = "The quick brown fox jumps over the lazy dog.";
+        string str2 = "This is a string";
+        string str3 = "THIS is a STRING";
+        string[] strs = { "one", "two", "three", "four" };
 
-// TODO: Length of a string 
+        // Length of a string
+        Console.WriteLine(str1.Length);
 
+        // Access individual characters
+        Console.WriteLine(str1[14]);
 
-// TODO: Access individual characters
+        // Iterate over a string like any other sequence of values
+        foreach (char ch in str1) {
+            Console.Write(ch);
+            if (ch == 'b') {
+                Console.WriteLine();
+                break;
+            }
+        }
 
+        // String Concatenation
+        string outstr;
+        outstr = String.Concat(strs);
+        Console.WriteLine(outstr);
 
-// TODO: Iterate over a string like any other sequence of values
+        // Joining strings together with Join
+        outstr = String.Join('.', strs);
+        Console.WriteLine(outstr);
 
+        // Corrected multi-line separator in Join method
+        outstr = String.Join("\n---\n", strs);
+        Console.WriteLine(outstr);
 
-// TODO: String Concatenation         
-string outstr;
+        // String Comparison
+        // Equals just returns a regular Boolean
+        bool isEqual = str2.Equals(str3);
+        Console.WriteLine($"{isEqual}");
 
+        // Compare will perform an ordinal comparison and return:
+        // < 0 : first string comes before second in sort order
+        // 0 : first and second strings are same position in sort order
+        // > 0 : first string comes after the second in sort order
+        int result = String.Compare(str2, "This is a string");
+        Console.WriteLine($"{result}");
 
-// TODO: Joining strings together with Join
-
-
-// TODO: String Comparison
-
-// Equals just returns a regular Boolean
-// bool isEqual = str2.Equals(str3);
-// Console.WriteLine($"{isEqual}");
-
-// Compare will perform an ordinal comparison and return:
-// < 0 : first string comes before second in sort order
-// 0 : first and second strings are same position in sort order
-// > 0 : first string comes after the second in sort order
-
-
-// TODO: Replacing content
+        // Replacing content
+        outstr = str1.Replace("fox", "cat");
+        Console.WriteLine($"{outstr}");
+    }
+}
